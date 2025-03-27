c24c3e1bf619ee3c38c00ebef7bd59070862b6c8
KoletoG <twindestroyersbgbg@gmail.com>
2025-03-27 08:21:18 +0200

    Add project files.

---
+++ Eye Health Helper V.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eye_Health_Helper_V._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
