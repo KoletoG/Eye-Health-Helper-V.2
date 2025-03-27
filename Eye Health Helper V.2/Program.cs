ab61f820cb742885d623ad4b7d3c0a4fcabc0d52
KoletoG <twindestroyersbgbg@gmail.com>
2025-03-27 08:54:35 +0200

    feat; Added a notification instance with text, showing itself every 20 minutes

--- Eye Health Helper V.2/Program.cs
+++ Eye Health Helper V.2/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CommunityToolkit.WinUI.Notifications;
 namespace Eye_Health_Helper_V._2
 {
     internal class Program
     {
+        private static ToastContentBuilder notification = new ToastContentBuilder();
         static void Main(string[] args)
         {
+            notification.AddText("EYE HEALTH");
+            notification.AddText("Please take a 20 second break,");
+            notification.AddText("staring at something 10 metres away");
+            notification.SetToastScenario(ToastScenario.Reminder);
+            notification.SetToastDuration(ToastDuration.Long);
+            notification.AddHeroImage(new Uri(""));
+            while (true)
+            {
+                notification.Show();
+                System.Threading.Thread.Sleep(12000000);
+            }
         }
     }
 }
