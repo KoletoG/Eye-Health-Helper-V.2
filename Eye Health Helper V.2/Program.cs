97fdd105b470aad9de955be9bac9560c634e769b
KoletoG <twindestroyersbgbg@gmail.com>
2025-03-27 09:07:50 +0200

    feat: Added asynchronous task so the app doesn't block the main thread

--- Eye Health Helper V.2/Program.cs
+++ Eye Health Helper V.2/Program.cs
@@ -1,26 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.WinUI.Notifications;
+using Microsoft.Win32;
 namespace Eye_Health_Helper_V._2
 {
     internal class Program
     {
         private static ToastContentBuilder notification = new ToastContentBuilder();
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             notification.AddText("EYE HEALTH");
             notification.AddText("Please take a 20 second break,");
             notification.AddText("staring at something 10 metres away");
             notification.SetToastScenario(ToastScenario.Reminder);
             notification.SetToastDuration(ToastDuration.Long);
-            notification.AddHeroImage(new Uri(""));
+            await ShowNotificationsPeriodically();
+        }
+        private static async Task ShowNotificationsPeriodically()
+        {
             while (true)
             {
                 notification.Show();
-                System.Threading.Thread.Sleep(12000000);
+                await Task.Delay(1200000); 
             }
         }
     }
