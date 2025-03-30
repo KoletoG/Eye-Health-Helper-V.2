8d910b9effb81f6ac1f48b33f31759219c4782f4
KoletoG <twindestroyersbgbg@gmail.com>
2025-03-30 11:12:19 +0300

    perf: Removed static variables, implemented a temporary instance of TemporaryContentBuilder
    Removed static TemporaryContentBuilder notifications and replaced them with a temporary one in ShowNotification() so it doesn't consume excess memory while in Task.Delay().

--- Eye Health Helper V.2/Program.cs
+++ Eye Health Helper V.2/Program.cs
@@ -10,32 +10,30 @@ namespace Eye_Health_Helper_V._2
 {
     internal class Program
     {
-        private static ToastContentBuilder notification = new ToastContentBuilder();
-        private static ToastContentBuilder doneNotification = new ToastContentBuilder();
         static async Task Main(string[] args)
         {
-            FillNotification(doneNotification, "You can now continue looking at the screen.", "Your eyes will thank you!", ToastDuration.Short);
-            FillNotification(notification, "Please take a 20 seconds break.", "You should look at something 10 meters away from you.", ToastDuration.Long);
             await ShowNotificationsPeriodically();
         }
         private static async Task ShowNotificationsPeriodically()
         {
             while (true)
             {
-                notification.Show();
+                ShowNotification("You can now continue looking at the screen.", "Your eyes will thank you!", ToastDuration.Short);
                 await Task.Delay(25000);
-                doneNotification.Show();
+                ShowNotification("Please take a 20 seconds break.", "You should look at something 10 meters away from you.", ToastDuration.Long);
                 await Task.Delay(1200000); 
             }
         }
-        private static void FillNotification(ToastContentBuilder notification, string text1, string text2, ToastDuration duration)
+        private static void ShowNotification(string text1, string text2, ToastDuration duration)
         {
-            notification.AddText("Eye Health");
-            notification.AddText(text1);
-            notification.AddText(text2);
-            notification.AddAppLogoOverride(new Uri(@"G:\VS Projects\Eye Health Helper V.2\Eye Health Helper V.2\bin\eye_health.png"));
-            notification.SetToastDuration(duration);
-            notification.SetToastScenario(ToastScenario.Reminder);
+            new ToastContentBuilder()
+            .AddText("Eye Health")
+            .AddText(text1)
+            .AddText(text2)
+            .AddAppLogoOverride(new Uri(@"G:\VS Projects\Eye Health Helper V.2\Eye Health Helper V.2\bin\eye_health.png"))
+            .SetToastDuration(duration)
+            .SetToastScenario(ToastScenario.Reminder)
+            .Show();
         }
     }
 }
