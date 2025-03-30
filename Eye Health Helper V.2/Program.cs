d7cf849882ac1a099b616406ab2944a82c18c63a
KoletoG <twindestroyersbgbg@gmail.com>
2025-03-30 10:23:18 +0300

    feat: Added a new notification in order to show user the 25 seconds timer has run off.
    Added a new notification which shows itself after 25 seconds as a reminder that those seconds have ended. It stays only for 7 seconds.

--- Eye Health Helper V.2/Program.cs
+++ Eye Health Helper V.2/Program.cs
@@ -11,6 +11,7 @@ namespace Eye_Health_Helper_V._2
     internal class Program
     {
         private static ToastContentBuilder notification = new ToastContentBuilder();
+        private static ToastContentBuilder doneNotification = new ToastContentBuilder();
         static async Task Main(string[] args)
         {
             notification.AddText("EYE HEALTH");
@@ -19,6 +20,12 @@ namespace Eye_Health_Helper_V._2
             notification.AddText("staring at something 10 metres away");
             notification.SetToastScenario(ToastScenario.Reminder);
             notification.SetToastDuration(ToastDuration.Long);
+            doneNotification.AddText("EYE HEALTH");
+            doneNotification.AddAppLogoOverride(new Uri(@"G:\VS Projects\Eye Health Helper V.2\Eye Health Helper V.2\bin\eye_health.png"));
+            doneNotification.AddText("You can now continue looking at the screen.");
+            doneNotification.AddText("Your eyes will thank you!");
+            doneNotification.SetToastDuration(ToastDuration.Short);
+            doneNotification.SetToastScenario(ToastScenario.Reminder);
             await ShowNotificationsPeriodically();
         }
         private static async Task ShowNotificationsPeriodically()
@@ -26,9 +33,10 @@ namespace Eye_Health_Helper_V._2
             while (true)
             {
                 notification.Show();
+                await Task.Delay(25000);
+                doneNotification.Show();
                 await Task.Delay(1200000); 
             }
         }
-        
     }
 }
