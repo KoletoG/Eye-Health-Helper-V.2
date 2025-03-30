6218ea71162ee8a1b90efe05db0183f7d4e847e4
KoletoG <twindestroyersbgbg@gmail.com>
2025-03-30 12:52:29 +0300

    feat: Added error handling
    Added simple error handling which shows user-friendly messages depending on the error.

--- Eye Health Helper V.2/Program.cs
+++ Eye Health Helper V.2/Program.cs
@@ -16,31 +16,64 @@ namespace Eye_Health_Helper_V._2
         private static readonly string fullPath = Path.GetFullPath("eye_health.png");
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Eye Health Helper is running... Press Enter to exit.");
-            _ = ShowNotificationsPeriodically(cts.Token);
-            Console.ReadLine();
-            cts.Cancel();
+            try
+            {
+                Console.WriteLine("Eye Health Helper is running... Press Enter to exit.");
+                _ = ShowNotificationsPeriodically(cts.Token);
+                Console.ReadLine();
+                cts.Cancel();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
         private static async Task ShowNotificationsPeriodically(CancellationToken cancellationToken)
         {
-            while (!cancellationToken.IsCancellationRequested)
+            try
             {
-                ShowNotification("Please take a 20 seconds break.", "You should look at something 10 meters away from you.", ToastDuration.Long);
-                await Task.Delay(25000,cancellationToken);
-                ShowNotification("You can now continue looking at the screen.", "Your eyes will thank you!", ToastDuration.Short);
-                await Task.Delay(1200000, cancellationToken); 
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    ShowNotification("Please take a 20 seconds break.", "You should look at something 10 meters away from you.", ToastDuration.Long);
+                    await Task.Delay(25000, cancellationToken);
+                    ShowNotification("You can now continue looking at the screen.", "Your eyes will thank you!", ToastDuration.Short);
+                    await Task.Delay(1200000, cancellationToken);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Application ended");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
         private static void ShowNotification(string text1, string text2, ToastDuration duration)
         {
-            new ToastContentBuilder()
-            .AddText("Eye Health")
-            .AddText(text1)
-            .AddText(text2)
-            .AddAppLogoOverride(new Uri(fullPath,UriKind.Absolute))
-            .SetToastDuration(duration)
-            .SetToastScenario(ToastScenario.Reminder)
-            .Show();
+            try
+            {
+                new ToastContentBuilder()
+                .AddText("Eye Health")
+                .AddText(text1)
+                .AddText(text2)
+                .AddAppLogoOverride(new Uri(fullPath, UriKind.Absolute))
+                .SetToastDuration(duration)
+                .SetToastScenario(ToastScenario.Reminder)
+                .Show();
+            }
+            catch(UriFormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
