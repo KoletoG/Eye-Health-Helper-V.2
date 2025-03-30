ad66cca45c1f4d07945e3737b4f46a441c46904e
KoletoG <twindestroyersbgbg@gmail.com>
2025-03-30 15:04:14 +0300

    fix: Added Dependency Injection
    Installed Microsoft.Extensions.DependencyInjection and Microsoft.Extensions.Logging. Made Notifications method into a NotificationsService class which inherits INotificationsService.

--- Eye Health Helper V.2/Program.cs
+++ Eye Health Helper V.2/Program.cs
@@ -7,98 +7,114 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using CommunityToolkit.WinUI.Notifications;
+using Microsoft.Extensions.Logging;
 using Microsoft.Win32;
 using Serilog;
+using Microsoft.Extensions.DependencyInjection;
+using System.Web.Services.Description;
+using static Eye_Health_Helper_V._2.Program;
 namespace Eye_Health_Helper_V._2
 {
     internal class Program
     {
-        private static readonly CancellationTokenSource cts;
-        private static readonly string fullPath; 
+        private static readonly IServiceProvider services;
         static Program()
         {
-            try
-            {
-                cts = new CancellationTokenSource();
-                fullPath = Path.GetFullPath("eye_health.png");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error initializing static fields: {e.Message}");
-                fullPath = string.Empty;
-            }
-        }
-        static async Task Main(string[] args)
-        {
+            var collection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
             Log.Logger = new LoggerConfiguration()
             .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
             .CreateLogger();
+            collection.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddSerilog(); // Use Serilog as the logging provider
+            });
+            collection.AddSingleton<INotificationsService, NotificationService>();
+            services=collection.BuildServiceProvider();
+        }
+        static async Task Main(string[] args)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            var notificationService = services.GetRequiredService<INotificationsService>();
             Log.Information("App started");
             try
             {
+                logger.LogInformation("App started");
+                var cts = new CancellationTokenSource();
+                _ = notificationService.ShowNotificationsPeriodically(cts.Token);
                 Console.WriteLine("Eye Health Helper is running... Press Enter to exit.");
-                _ = ShowNotificationsPeriodically(cts.Token);
                 Console.ReadLine();
                 cts.Cancel();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                Log.Error(e.ToString());
+                logger.LogError(e.ToString());
             }
             finally
             {
-                Log.Information("App has stopped");
+               logger.LogInformation("App has stopped");
                 Log.CloseAndFlush();
             }
         }
-        private static async Task ShowNotificationsPeriodically(CancellationToken cancellationToken)
+        interface INotificationsService
         {
-            try
+            Task ShowNotificationsPeriodically(CancellationToken token);
+        }
+        public class NotificationService : INotificationsService
+        {
+            private readonly ILogger<NotificationService> _logger;
+            private readonly string _fullPath;
+            public NotificationService(ILogger<NotificationService> logger)
             {
-                while (!cancellationToken.IsCancellationRequested)
+                _logger = logger;
+                _fullPath = Path.GetFullPath("eye_health.png");
+            }
+            public async Task ShowNotificationsPeriodically(CancellationToken cancellationToken)
+            {
+                try
                 {
-                    ShowNotification("Please take a 20 seconds break.", "You should look at something 10 meters away from you.", ToastDuration.Long);
-                    await Task.Delay(25000, cancellationToken);
-                    ShowNotification("You can now continue looking at the screen.", "Your eyes will thank you!", ToastDuration.Short);
-                    await Task.Delay(1200000, cancellationToken);
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        ShowNotification("Please take a 20 seconds break.", "You should look at something 10 meters away from you.", ToastDuration.Long);
+                        await Task.Delay(25000, cancellationToken);
+                        ShowNotification("You can now continue looking at the screen.", "Your eyes will thank you!", ToastDuration.Short);
+                        await Task.Delay(1200000, cancellationToken);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    _logger.LogInformation("Service has been cancelled");
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.ToString());
                 }
             }
-            catch (OperationCanceledException)
+            private void ShowNotification(string text1, string text2, ToastDuration duration)
             {
-                Console.WriteLine("Application has stopped");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Log.Error(e.ToString());
-            }
-        }
-        private static void ShowNotification(string text1, string text2, ToastDuration duration)
-        {
-            try
-            {
-                new ToastContentBuilder()
-                .AddText("Eye Health")
-                .AddText(text1)
-                .AddText(text2)
-                .AddAppLogoOverride(new Uri(fullPath, UriKind.Absolute))
-                .SetToastDuration(duration)
-                .SetToastScenario(ToastScenario.Reminder)
-                .Show();
-            }
-            catch(UriFormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch(FileNotFoundException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Log.Error(e.ToString());
+                try
+                {
+                    new ToastContentBuilder()
+                    .AddText("Eye Health")
+                    .AddText(text1)
+                    .AddText(text2)
+                    .AddAppLogoOverride(new Uri(_fullPath, UriKind.Absolute))
+                    .SetToastDuration(duration)
+                    .SetToastScenario(ToastScenario.Reminder)
+                    .Show();
+                }
+                catch (UriFormatException e)
+                {
+                    _logger.LogError(e.Message);
+                }
+                catch (FileNotFoundException e)
+                {
+                    _logger.LogError(e.Message);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.ToString());
+                }
             }
         }
     }
